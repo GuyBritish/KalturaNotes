0d890ea6079550b7fb7f08fa314d4a8bb6dc90b8
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-23 16:16:37 -0500

    Create video component

---
+++ kalturanotes/src/Video/VideoPlayer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./VideoPlayer.module.css";
+
+const VideoPlayer = (props) => {
+	return <h1> This is our video</h1>;
+};
+
+export default VideoPlayer;
