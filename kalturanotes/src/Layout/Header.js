76e9db5944e45902be7857c07da498ed62a3453c
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-23 16:34:08 -0500

    Header component

---
+++ kalturanotes/src/Layout/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+	return <React.Fragment></React.Fragment>;
+};
+
+export default Header;
--- kalturanotes/src/Video/VideoPlayer.js
+++ kalturanotes/src/Video/VideoPlayer.js
@@ -3,7 +3,7 @@ import React from "react";
 import classes from "./VideoPlayer.module.css";
 
 const VideoPlayer = (props) => {
-	return <h1> This is our video</h1>;
+	return <React.Fragment></React.Fragment>;
 };
 
 export default VideoPlayer;
